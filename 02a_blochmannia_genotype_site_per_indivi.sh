0019bb38c0a4ba40eec70354e4f8452b72566d8c
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-07-16 07:50:55 -0500

    Create 02a_blochmannia_genotype_site_per_indivi.sh

---
+++ 02a_blochmannia_genotype_site_per_indivi.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=total_sites
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --partition=nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+
+cd /lustre/scratch/sboyane/camplaevi/01_blochmannia/03_vcf
+
+###total genotyped sites per individual
+
+for file in *.gz; do
+  count=$(zcat "$file" | wc -l)
+  echo "$file: $count" >> total_sites_all.txt
+done
