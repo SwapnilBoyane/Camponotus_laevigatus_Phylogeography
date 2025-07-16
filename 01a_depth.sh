bd2d9d87314fe384f2795320026ea2586fa86ae6
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-07-15 23:39:41 -0500

    Create 01a_depth_camplaevi.sh

---
+++ 01a_depth.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=genotype
+#SBATCH --partition nocona
+#SBATCH --nodes=1 --ntasks=4
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-30
+
+
+conda activate samtools
+
+# Set working directory
+workdir=/lustre/scratch/sboyane/camplaevi
+
+basename_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/basenames.txt | tail -n1 )
+
+# Calculate the depth of coverage per site
+samtools depth -a ${workdir}/01_bam_files/${basename_array}_final.bam > ${workdir}/depth/${basename_array}_depth.txt
+
+# Isolate the third column with the read counts only to reduce file size
+cut -f3 ${workdir}/depth/${basename_array}_depth.txt > ${workdir}/depth/${basename_array}_depth_reduced.txt
+
+# Remove the full depth file
+rm ${workdir}/depth/${basename_array}_depth.txt
