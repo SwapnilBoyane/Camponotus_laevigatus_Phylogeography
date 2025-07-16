8d4b818b72f1167a5478da19419afa192d6ed2ae
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-07-15 21:36:06 -0500

    Update 03_Bloch_filter.sh

--- 03_Bloch_filter.sh
+++ 03_Bloch_filter.sh
@@ -10,15 +10,15 @@ module load intel vcftools
 module load intel bcftools
 
 # define main working directory
-workdir=/lustre/scratch/sboyane/blochmannia
+workdir=/lustre/scratch/sboyane/C_laevigatus/01_blochmannia
 
 #run vcftools with SNP output spaced 20kbp
 #for PCA, EEMS, IBD 
-vcftools --vcf ${workdir}/03_vcf/blochmannia_final.g.vcf --keep keeplist_pca.txt --max-missing 1.0   --minDP 6  --min-alleles 2 --max-alleles 2 --mac 2  --max-maf 0.49  --recode --recode-INFO-all --out ${workdir}/04_filtered_vcf/blochmannia_final
+vcftools --vcf ${workdir}/03_vcf/blochmannia_final.g.vcf --keep keeplist_pca.txt --max-missing 1.0   --minDP 10  --min-alleles 2 --max-alleles 2 --mac 2  --max-maf 0.49  --recode --recode-INFO-all --out ${workdir}/04_filtered_vcf/blochmannia_final
 
 # run vcftools with SNP and invariant site output, 20% max missing data, no indels 
 #for Phylogeny 
-vcftools --vcf ${workdir}/03_vcf/blochmannia_final.g.vcf --max-missing 0.8 --minGQ 20 --minDP 6  --max-alleles 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_phylo_vcf/blochmannia_final_phylo
+vcftools --vcf ${workdir}/03_vcf/blochmannia_final.g.vcf --max-missing 0.8 --minGQ 20 --minDP 10  --max-alleles 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_phylo_vcf/blochmannia_final_phylo
 
 
 
