14993357d57128a88b75c811b67c316509bac7f1
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2026-03-09 00:03:31 -0500

    Create _01_make_GADMA_input.sh

---
+++ GADMA/_01_make_GADMA_input.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --mem-per-cpu=64G
+#SBATCH --partition=quanah
+#SBATCH --time=48:00:00
+#SBATCH --job-name=easySFS
+
+cd /lustre/scratch/sboyane/camplaevi/gadma
+
+source activate easySFS
+
+# run easySFS, first in preview mode then for real based on the preview
+# projection based on downsampled (1 snp per chrom) with Z was --proj=10,10,20
+
+~/easySFS/easySFS.py \
+  -i combined_10mis_variant.vcf \
+  -p popmap_gadma.txt \
+  -o easysfs_final_nov_101020 \
+  -a --proj=10,10,20 --total-length 184409011
